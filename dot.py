9ab00b236c9f11b5638eab6044f567329c0ddc7b
nomi3 <thisisnomi3@gmail.com>
2022-09-25 17:59:53 +0900

    ランダムで400個の点を選ぶように変更

--- dot.py
+++ dot.py
@@ -1,19 +1,26 @@
 import numpy as np
+import random
 
-test = np.random.randint(2, size=(24, 24))
+def rand_ints_nodup(a, b, k):
+  ns = []
+  while len(ns) < k:
+    n = random.randint(a, b)
+    if not n in ns:
+      ns.append(n)
+  return ns
 
-enemy_img = [
-    [0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-    [1,1,0,0,1,0,0,0,0,0,1,0,0,0,1,1],
-    [1,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1],
-    [1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,1],
-    [0,0,1,1,0,1,1,1,0,1,1,0,1,0,1,0],
-    [0,0,0,1,1,1,0,0,0,0,0,1,1,1,0,0],
-    [0,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1],
-    [0,0,0,1,1,1,1,0,0,0,0,0,1,1,1,1]
-]
+rand_num = rand_ints_nodup(0, 24*24-1, 400)
 
-for line in test:
+test = np.zeros(24*24, dtype=int)
+
+for i in range(len(test)):
+    print(i)
+    if i in rand_num:
+        test[i] = 1
+
+test2 = test.reshape(24, 24)
+
+for line in test2:
     for dot in line:
         if dot == 1:
             print("#", end="")
